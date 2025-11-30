0d9bc6bf90e1594224258e2fde73677c6d283a8c
abdullahejazjanjua <abdullahejaz822@gmail.com>
2025-11-30 23:36:03 +0500

    Added form code

--- webapp/app/views.py
+++ webapp/app/views.py
@@ -1,6 +1,16 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .forms import LoginForm
 
 # Create your views here.
 def login_form(request):
-    return HttpResponse("Hello, World")
+
+    form = LoginForm()
+
+    if request.method == "POST":
+        form = LoginForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return HttpResponse("Success!")
+
+    return render(request, "index.html", {'form': form})
